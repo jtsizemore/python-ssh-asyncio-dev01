be7e66b0e5ccad720e6cc4f1fe07abd5b06d1b6f
jtsizemore <jtsizemore@outlook.com>
2023-04-20 02:14:10 +0000

    working on paramiko ssh client invoke shell module

--- dev02.py
+++ dev02.py
@@ -12,6 +12,7 @@ import yaml
 import re
 
 
+# paramiko ssh client
 def ssh_connect(input_dict: dict) -> bytes:
     """
     Dev
@@ -20,53 +21,95 @@ def ssh_connect(input_dict: dict) -> bytes:
     ssh_client.load_system_host_keys()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(
-        input_dict["hostname"],
-        username=input_dict["username"],
-        password=input_dict["password"]
+        input_dict['hostname'],
+        username=input_dict['username'],
+        password=input_dict['password']
         )
-    stdin, stdout, stderr = ssh_client.exec_command(input_dict["cli_cmd"])
-    time.sleep(5)
-    ssh_client.close()
-    ssh_output_bytes = stdout.read()
+    # verify:
+    #   - 'channel_mode' is string
+    #   - 'channel_mode' lower case equals 'exec_command'
+    #   - in case 'cli_cmd' list > 0, only take first index of 0
+    if isinstance(input_dict['channel_mode'], str) and \
+        input_dict['channel_mode'].lower() == 'exec_command':
+        print("exec mode")
+        stdin, stdout, stderr = \
+            ssh_client.exec_command(input_dict['cli_cmd'][0])
+        time.sleep(5)
+        ssh_client.close()
+        ssh_output_bytes = stdout.read()
+        h = input_dict['hostname'].replace('.', '_')
+        d = {'hostname': h, 'cli_output': ssh_output_bytes}
+        return d
+    # verify:
+    #   - 'channel_mode' is string
+    #   - 'channel_mode' lower case equals 'invoke_shell'
+    elif isinstance(input_dict['channel_mode'], str) and \
+        input_dict['channel_mode'].lower() == 'invoke_shell':
+        print("invoke shell")
+        rx_bytes_list = []
+        paramiko_ssh_channel = ssh_client.invoke_shell()
+        if paramiko_ssh_channel.send_ready():
+            paramiko_ssh_channel.send('\nterminal length 0\n')
+            for cmd in input_dict['cli_cmd']:
+                send_str = f'{str(cmd)}\n'
+                paramiko_ssh_channel.send(send_str)
+                time.sleep(0.5)
+                if paramiko_ssh_channel.recv_ready():
+                    while paramiko_ssh_channel.recv_ready():
+                        rx_bytes = paramiko_ssh_channel.recv(1024e6)
+                        rx_bytes_list.append(rx_bytes)
+                    time.sleep(5)
+            h = input_dict['hostname'].replace('.', '_')
+            d = {'hostname': h, 'cli_output': rx_bytes_list}
+        return d
 
-    h = input_dict['hostname'].replace('.', '_')
-    d = {'hostname': h, 'cli_output': ssh_output_bytes}
 
-    return d
-
-
-def write_file(input_dict: str, file_path: str='no_git_tmp', write_mode: str='w') -> None:
+# write file from input dict
+def write_file(
+    input_dict: dict, file_path: str='no_git_tmp', write_mode: str='w'
+    ) -> None:
     """
     Dev
     """
     file_name = f"{file_path}/{input_dict['hostname']}.txt"
-    cli_output = input_dict['cli_output'].decode('utf-8')
-    with open(file_name, write_mode) as f:
-        f.write(cli_output)
+    if isinstance(input_dict['cli_output'], bytes):
+        print("bytes")
+        cli_output = input_dict['cli_output'].decode('utf-8')
+        with open(file_name, write_mode) as f:
+            f.write(cli_output)
+    elif isinstance(input_dict['cli_output'], list):
+        print("list")
+        with open(file_name, write_mode) as f:
+            f.writelines(cli_output.decode('utf-8'))
 
 
+# read yaml and convert to python obj
 def yaml_func(input_file: str, file_mode: str='r'):
     """
     Dev
     """
     with open(input_file, file_mode) as f:
         s = f.read()
+
     return yaml.safe_load(s)
 
 
+# argparse
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'file', help='fully qualified/absolute path to input Yaml file'
     )
 args = parser.parse_args()
 argparse_error_msg = \
-'\nInvalid File Extension {}. Must be "yaml" or "yml" (case insensitive).'
+    '\nInvalid File Extension {}. Must be "yaml" or "yml" (case insensitive).'
 error_str = '{}{}{}'.format('"', args.file.split('.')[-1], '"')
 
+# regex for argparse
 r_str = r"^(\w|\-|/|\.|\:|\s)+(\w|\-|/|\.|\:|\s)*\.(yaml|yml)$"
 r = re.compile(r_str, re.IGNORECASE)
 
 
+# run
 if __name__ == '__main__':
     if r.match(args.file):
         print(f"Input File Path: {args.file}")
